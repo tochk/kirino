09be260db407400fee6f4271535694257939bbed
Alexey Andreev <bezumny.tochk@gmail.com>
2016-11-18 20:17:23 +0300

    Added file showing

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"text/template"
 	"os"
 	"os/exec"
+	"crypto/sha256"
+	"encoding/hex"
+	"math/rand"
+	"time"
+	"strconv"
+	"fmt"
 )
 
 var config struct {
@@ -44,18 +50,29 @@ func loadConfig() error {
 	return json.Unmarshal(jsonData, &config)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Loaded index page from %s", r.RemoteAddr)
-	tmpl := template.New("Index page template")
-	tmpl, err := template.ParseFiles("layouts/base.tmpl")
+func generatePdfHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Loaded generatePdf page from %s", r.RemoteAddr)
+	err := r.ParseForm()
 	if err != nil {
 		log.Fatal(err)
 	}
-	temp := Temp{Content: "tochk"}
-	err = tmpl.ExecuteTemplate(w, "base.tmpl", temp)
-	if err != nil {
-		log.Fatal(err)
+	var list []UserData
+	for i := 1; i <= len(r.Form) / 3; i++ {
+		tempData := UserData{MacAddr: r.PostFormValue("mac" + strconv.Itoa(i)), UserName: r.PostFormValue("user" + strconv.Itoa(i)), PhoneNumber: r.PostFormValue("tel" + strconv.Itoa(i))}
+		list = append(list, tempData)
 	}
+	hasher := sha256.New()
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	hashStr := r.PostFormValue("mac1") + strconv.Itoa(r1.Intn(1000000))
+	log.Print(hashStr)
+	hasher.Write([]byte(hashStr))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	log.Print(hash)
+	pathToTex := generateLatexFile(list, hash)
+	generatePdf(pathToTex)
+	log.Print(list)
+	fmt.Fprint(w, "<a href='/userFiles/" + hash + ".pdf'>Link</a>")
 }
 
 func generateLatexTable(list []UserData) Table {
@@ -67,7 +84,7 @@ func generateLatexTable(list []UserData) Table {
 	return Table{Table: table}
 }
 
-func generateLatexFile(list []UserData, hashStr string) {
+func generateLatexFile(list []UserData, hashStr string) string {
 	log.Print("Generating latex file")
 	latexTemplate := template.New("Latex template")
 	latexTemplate, err := template.ParseFiles("latex/wifi.tex")
@@ -78,13 +95,18 @@ func generateLatexFile(list []UserData, hashStr string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer outputLatexFile.Close()
 	err = latexTemplate.ExecuteTemplate(outputLatexFile, "wifi.tex", generateLatexTable(list))
 	if err != nil {
 		log.Fatal(err)
 	}
-	outputLatexFile.Close()
+
 	pathToTexFile := "userFiles\\" + hashStr + ".tex"
-	cmd := exec.Command("pdflatex", "--interaction=errorstopmode", "--synctex=-1", "-output-directory=userFiles", pathToTexFile)
+	return pathToTexFile
+}
+
+func generatePdf(path string) {
+	cmd := exec.Command("pdflatex", "--interaction=errorstopmode", "--synctex=-1", "-output-directory=userFiles", path)
 	cmd.Start()
 }
 
@@ -101,6 +123,8 @@ func main() {
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
+	fs2 := http.FileServer(http.Dir("userFiles"))
+	http.Handle("/userFiles/", http.StripPrefix("/userFiles/", fs2))
+	http.HandleFunc("/generatePdf/", generatePdfHandler)
 	http.ListenAndServe(":8080", nil)
 }
