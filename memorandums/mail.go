4325ad026d8474b06f98d141c884544552b04352
Alexey Andreev <bezumny.tochk@gmail.com>
2018-06-10 15:36:21 +0400

    Add showing mail memorandums

--- memorandums/mail.go
+++ memorandums/mail.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/tochk/kirino/auth"
+	"github.com/tochk/kirino/pagination"
+	"github.com/tochk/kirino/server"
 	"github.com/tochk/kirino/templates/html"
 )
+
+type MailMemorandum = html.MailMemorandum
 
 func MailHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Loaded %s page from %s", r.URL.Path, r.Header.Get("X-Real-IP"))
@@ -20,12 +26,71 @@ func MailHandler(w http.ResponseWriter, r *http.Request) {
 
 func ListMailHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Loaded %s page from %s", r.URL.Path, r.Header.Get("X-Real-IP"))
-	if auth.IsAdmin(r) {
-		fmt.Fprint(w, html.MailPage("admin"))
-	} else {
-		fmt.Fprint(w, html.MailPage("mail"))
+	session, _ := server.Core.Store.Get(r, "kirino_session")
+	if session.Values["userName"] == nil {
+		http.Redirect(w, r, "/admin/", 302)
+		return
 	}
+	var paging pagination.Pagination
+	var memorandums []MailMemorandum
+	var err error
+	count, err := getMailCount()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	r.ParseForm()
+	urlInfo := r.URL.Path[len("/admin/mail/memorandums/"):]
+	splittedUrl := strings.Split(urlInfo, "/")
+	switch splittedUrl[0] {
+	case "page":
+		page, err := strconv.Atoi(splittedUrl[1])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		paging = pagination.Calc(page, count)
+		memorandums, err = getMailMemorandums(paging.PerPage, paging.Offset)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	case "accept":
+		_, err := server.Core.Db.Exec("UPDATE mailmemorandums SET accepted = 1 WHERE id = $1", splittedUrl[1])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		http.Redirect(w, r, "/admin/mail/memorandums/", 302)
+		return
+	default:
+		if paging.CurrentPage == 0 {
+			memorandums, err = getMailMemorandums(50, 0)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			paging = pagination.Calc(1, count)
+		}
+	}
+
+	for index, memorandum := range memorandums {
+		memorandums[index].AddTime = strings.Split(memorandum.AddTime, "T")[0]
+	}
+
+	fmt.Fprint(w, html.MailMemorandumsPage(memorandums, paging))
 }
+
+func getMailMemorandums(limit, offset int) (domains []MailMemorandum, err error) {
+	err = server.Core.Db.Select(&domains, "SELECT * FROM mailmemorandums ORDER BY id DESC LIMIT $1 OFFSET $2 ", limit, offset)
+	return
+}
+
+func getMailCount() (count int, err error) {
+	err = server.Core.Db.Get(&count, "SELECT COUNT(*) FROM mailmemorandums")
+	return
+}
+
 
 func ViewMailHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Loaded %s page from %s", r.URL.Path, r.Header.Get("X-Real-IP"))
